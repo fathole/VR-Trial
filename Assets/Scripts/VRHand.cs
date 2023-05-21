cad28b8984e8cdb747c7b97bee6c46252774370e
TW <tomwonglokho@gmail.com>
2023-05-21 23:00:03 +0800

    1.0.0

--- Assets/Scripts/VRHand.cs
+++ Assets/Scripts/VRHand.cs
@@ -17,5 +17,17 @@ public class VRHand : MonoBehaviour
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
         animator.SetFloat("Grip", triggerValue);
+
+        //// Old Input System
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    Debug.Log("Called");
+        //}
+
+        // New Input System
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            Debug.Log("Called");
+        }
     }
 }
